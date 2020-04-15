9738e5bcd5cb8f77bc6d3ce1ac15515508a4ef42
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-15 13:11:32 -0500

    funciones puras

--- ejemplo.js
+++ ejemplo.js
@@ -12,3 +12,12 @@ const double = (x) => {
 const double = x => x * 2
 
 console.log(double(3))
+
+//funcion pura: siempre entrega el mismo resultado a partir del mismo argumente que reciba
+const isGreaterThan = (value, comparison) => value > comparison
+console.log(isGreaterThan(5,6))
+console.log(isGreaterThan(7,3))
+
+//funcion NO pura: nos entregará diferentes resultados cada vez que la mandemos llamar
+const time = () => new Date().toLocaleTimeString()
+console.log(time())
