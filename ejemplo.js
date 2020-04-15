79ad99402661e0b29e25dd2e0509b6912ccc29b3
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-15 11:39:37 -0500

    ejemplo arrow function

---
+++ ejemplo.js
@@ -0,0 +1,14 @@
+//Normal function
+function double(x) {
+  return x * 2;
+}
+
+//Arrow function
+const double = (x) => {
+  return x * 2;
+};
+
+//Short arrow function
+const double = x => x * 2
+
+console.log(double(3))
